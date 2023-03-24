2e651ef1cb59ff60b99b4b7f179628f84aef0633
Abdulganiyu Kamaldeen <abdulganiyykamaldeen@gmail.com>
2023-03-24 09:16:02 -0700

    Correction for task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
-#Creates a file in tmp
+# Create a file in /tmp
+
 file { 'holberton':
-  ensure  => 'file',
-  path    => '/tmp/holberton',
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
-  }
+  path    => '/tmp/holberton',
+}
